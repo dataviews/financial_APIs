51a1fa267ce3ca8f1ea2b1796e0c8c148769f507
dataviews <lambersy@gmail.com>
2020-11-08 22:06:31 +0100

    update, seaborn has issue sin venv

---
+++ stocks.py
@@ -0,0 +1,11 @@
+tech = {"MSFT": "Microsoft",
+        "BABA": "Alibaba",
+        "SNOW": "Snowflake",
+        "IBM": "IBM",
+        "AMZN": "Microsoft",
+        "TSLA": "Tesla"}
+
+pharma = {"AZN": "AstraZeneka",
+        "ROG:SIX": "Roche",
+        "NVS": "Novartis",
+        }
--- yahoo_finance.py
+++ yahoo_finance.py
@@ -4,17 +4,16 @@ import ssl
 import datetime
 import time
 import seaborn as sns
+import matplot.pyplot as plt
 import numpy as np
 import warnings
+from stocks import *
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
 # documentation here: https://github.com/ranaroussi/yfinance
-
-tickers = ["MSFT", "BABA", "IBM"]
-
 
 def include_keys(dictionary, keys):
     """Filters a dict by only including certain keys."""
@@ -29,8 +28,8 @@ def get_data(ticker):
     # get stock info
     info = comp.info
 
-    print(np.mean(comp.dividends))
     hist_div = comp.dividends
+    mean_dividend = np.mean(comp.dividends)
 
     keep = ['regularMarketPreviousClose', 'industry', 'lastDividendValue', 'lastDividendDate',
             'fiveYearAvgDividendYield', 'dividendRate']
@@ -46,10 +45,21 @@ def get_data(ticker):
     # df['lastDividendDate'] = datetime.datetime.fromtimestamp(df['lastDividendDate']).isoformat()
     # df['lastDividendDate'] = pd.to_datetime(df['lastDividendDate'], format='%Y-%m-%dT%H:%M:%S')
 
-    sns.distplot(hist_div, label=ticker)
-
-    return df, info, hist_div
+    return df, info, hist_div, mean_dividend
 
 
-for ticker in tickers:
-    get_data(ticker)
+for i, ticker in enumerate(tech):
+    df, info, hist_div, mean_dividend = get_data(ticker)
+
+    if len(hist_div) == 0:
+        print("No dividends to graph")
+        pass
+    else:
+        # plt.figure(i)
+        # ax = sns.distplot(hist_div, bins = 50)
+        # ax.set_title("Stock " + ticker)
+
+        fig = plt.figure()
+        ax = plt.axes()
+        ax.plot(hist_div)
+        ax.set_title("Stock " + ticker)
